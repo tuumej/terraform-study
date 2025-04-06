7347cfbf1922ef43bda0bd61c7c979fbdb99c765
tuumej <tuumej@gmail.com>
2025-04-06 22:58:45 +0900

    ncp infra achi add

--- ncp/prd-archi-ex-1/main.tf
+++ ncp/prd-archi-ex-1/main.tf
@@ -20,9 +20,55 @@ resource "ncloud_vpc" "prd_vpc" {
   ipv4_cidr_block = var.prd_vpc_ip
 }
 
-resource "ncloud_access_control_group" "access_control_group" {
-  description = ""
-  name        = ""
-  vpc_no      = ncloud_vpc.prd_vpc.vpc_no
+resource "ncloud_subnet" "prd_subnet" {
+  name           = var.prd_pub_sub_name[0]
+  network_acl_no = ncloud_network_acl.network_acl.id
+  subnet         = var.prd_sub_ip[1]
+  subnet_type    = "PUBLIC"
+  usage_type     = "GEN"
+  vpc_no         = ncloud_vpc.prd_vpc.id
+  zone           = "KR-1"
+}
 
+resource "ncloud_access_control_group" "access_control_group" {
+  description = "default port open acg config"
+  name        = var.acg_name[0]
+  vpc_no      = ncloud_vpc.prd_vpc.vpc_no
+}
+
+# acg icmp > port_range ??
+resource "ncloud_access_control_group_rule" "access_control_group_rule" {
+  access_control_group_no = ncloud_access_control_group.access_control_group.id
+  inbound = [{
+    protocol                       = "TCP"
+    ip_block                       = "193.186.4.0/24"
+    source_access_control_group_no = ""
+    port_range                     = "22"
+    description                    = "sanchez cafe icmp test"
+  }]
+  outbound = [{
+    protocol                       = "TCP"
+    ip_block                       = "0.0.0.0/0"
+    source_access_control_group_no = ""
+    port_range                     = "1-65535"
+    description                    = "tcp outbound ports"
+    },
+    {
+      protocol                       = "UDP"
+      ip_block                       = "0.0.0.0/0"
+      source_access_control_group_no = ""
+      port_range                     = "1-65535"
+      description                    = "udp outbound ports"
+  }]
+}
+
+# nacl
+resource "ncloud_network_acl" "network_acl" {
+  description = "default nacl"
+  name        = "test-nacl"
+  vpc_no      = ncloud_vpc.prd_vpc.id
+}
+
+resource "ncloud_network_acl_rule" "nacl_rule" {
+  network_acl_no = ncloud_network_acl.network_acl.id
 }
